c732e8b76aa0c0aa116d6127914bf2ba80f6a532
Abdulrazack <143405260+dulrazack@users.noreply.github.com>
2024-05-24 12:26:00 +0300

    Create Java 4.java

---
+++ src/Java 4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class CodesCracker4
+{
+    public static void main(String[] args)
+    {
+        int num;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter an Integer Value: ");
+        num = scan.nextInt();
+
+        System.out.println("\nYou've entered: " +num);
+    }
+}
