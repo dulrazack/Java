ed79a418477b44e2a30cec54fc022622b359fed5
Abdulrazack <143405260+dulrazack@users.noreply.github.com>
2024-05-24 12:29:29 +0300

    Create Java 7.java

---
+++ src/Java 7.java
@@ -0,0 +1,19 @@
+
+/*Shutdown PC*/
+import java.io.*;
+public class CodesCracker7
+{
+    public static void main(String[] args)
+    {
+        Runtime runtime = Runtime.getRuntime();
+        try
+        {
+            System.out.println("Shutting down the PC after 5 seconds.");
+            runtime.exec("shutdown -s -t 5");
+        }
+        catch(IOException e)
+        {
+            System.out.println("Exception: " +e);
+        }
+    }
+}
